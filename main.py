551107b3114a9e9b4205aa936c3963c1fbabe510
Wojtsik <72934201+Wojtsik@users.noreply.github.com>
2023-03-28 20:10:55 +0200

    Update main.py

--- main.py
+++ main.py
@@ -27,5 +27,5 @@ async def on_message(message):
                 channel = bot.get_channel(954711698957291531)
                 await channel.send(attachment.url)
         await bot.process_commands(message)
-token = os.environ['token']
+token = ./
 bot.run(token)
